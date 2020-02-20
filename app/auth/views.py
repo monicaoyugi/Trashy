6dafb4324ef558b4aad36acf998bcf8ad39d5180
Marionjames-ally <marionjames2018@gmail.com>
2020-02-20 21:20:22 +0300

    create the registration form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,15 +1,12 @@
 from flask import render_template, redirect, url_for, flash, request
 from . import auth
 from ..models import User
-from .forms import RegistrationForm, LoginForm
 from .. import db
 from flask_login import login_user, login_required, logout_user,current_user
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    login_form = LoginForm()
-
     if login_form.validate_on_submit():
         user = User.query.filter_by(email=login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
@@ -19,15 +16,12 @@ def login():
         flash('Invalid username or Password')
 
     title = "Blog-X Login"
-    return render_template('auth/login.html', login_form=login_form, title=title)
+    return render_template('auth/login.html', title=title)
 
 
 @auth.route('/register', methods=["GET", "POST"])
 def register():
-    form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data,
-                    username=form.username.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
 
